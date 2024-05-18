2842c4369aaa261b420cdca003cb03019fdbfb37
ecarnovsky <ecarnovsky@gmail.com>
2024-05-18 13:32:51 -0400

    Changed the request mapping to use the get method

--- src/main/java/com/example/csi2999/MainController.java
+++ src/main/java/com/example/csi2999/MainController.java
@@ -2,19 +2,20 @@ package com.example.csi2999;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 
 @Controller
 public class MainController {
     
-    @RequestMapping("/")
+    @RequestMapping(method = RequestMethod.GET, value = "/")
     public String home() {
 
       return "index";
       
     }
 
-    @RequestMapping("/about")
+    @RequestMapping(method = RequestMethod.GET, value = "/about")
     public String about() {
 
       return "about";

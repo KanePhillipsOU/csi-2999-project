21919dc0cd9a26e4c6b0acfe29f7f4e51a1bf401
Emily Lasecki <emilylasecki@oakland.edu>
2024-05-20 19:07:31 -0400

    made buttons to navigate between pages

--- src/main/java/com/example/csi2999/MainController.java
+++ src/main/java/com/example/csi2999/MainController.java
@@ -7,11 +7,20 @@ import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class MainController {
+
+
+
+  @RequestMapping(method = RequestMethod.GET, value = "/")
+  public String startup() {
+
+    return "reservations";  //will navigate to home, but for sake of readability take to reservations for now
     
-    @RequestMapping(method = RequestMethod.GET, value = "/")
+  }
+    
+    @RequestMapping(method = RequestMethod.GET, value = "/home")
     public String home() {
 
-      return "index";
+      return "home";   //I will get rid od this method later too
       
     }
 
@@ -21,5 +30,19 @@ public class MainController {
       return "about";
       
     }
+
+    @RequestMapping(method = RequestMethod.GET, value = "/moreparks")
+    public String moreparks() {
+
+      return "moreparks";
+    }
+      
+
+       @RequestMapping(method = RequestMethod.GET, value = "/reservations")
+    public String reservations() {
+
+      return "reservations"; 
+    } 
+
 
 }

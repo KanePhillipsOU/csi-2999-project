ee2271238beb70cd16b8d0e10567d4b2646c4696
ecarnovsky <ecarnovsky@gmail.com>
2024-05-22 01:20:20 -0400

    Removed an import

--- src/main/java/com/example/csi2999/SupabaseClient.java
+++ src/main/java/com/example/csi2999/SupabaseClient.java
@@ -13,7 +13,6 @@ import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
-import jakarta.annotation.PostConstruct;
 
 @Service
 public class SupabaseClient {

beeeb0a892bd058063ef56fa2b8ceacc843705e1
ecarnovsky <ecarnovsky@gmail.com>
2024-05-21 22:42:42 -0400

    Created the SupabaseClient class

---
+++ src/main/java/com/example/csi2999/SupabaseClient.java
@@ -0,0 +1,22 @@
+package com.example.csi2999;
+
+import org.json.JSONObject;
+
+public class SupabaseClient {
+
+    private final String url = " ";
+    private final String apiKey = " "; 
+    private final String authToken = " ";
+
+    public JSONObject getReservations(){
+        return new JSONObject();
+    }
+
+    public JSONObject getSites(){
+        return new JSONObject();
+    }
+
+    private JSONObject makeHTTPRequest(String str){
+        return new JSONObject();
+    }
+}

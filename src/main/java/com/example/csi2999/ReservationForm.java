9d2f011b708c6d3bc384375161a103bfc33d7034
ecarnovsky <ecarnovsky@gmail.com>
2024-05-23 17:51:05 -0400

    Added documentation comments

--- src/main/java/com/example/csi2999/ReservationForm.java
+++ src/main/java/com/example/csi2999/ReservationForm.java
@@ -1,7 +1,11 @@
 package com.example.csi2999;
 
+/**
+ * This class is used to represent the form information that 
+ * is used for making a reservation
+ */
 public class ReservationForm {
-    
+
 
     private String firstName;
     private String lastName;

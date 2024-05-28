2fbde3a65eae62736eec48b526f33b7fe8c2390b
KanePhillips <kanephillips@oakland.edu>
2024-05-27 21:27:09 -0400

    added thymeleaf formatting to search using example sites

--- src/main/java/com/example/csi2999/MainController.java
+++ src/main/java/com/example/csi2999/MainController.java
@@ -6,6 +6,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+//import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -64,10 +66,22 @@ public class MainController {
 
     
     @RequestMapping(method = RequestMethod.GET, value = "/search")
-    public String getSearch() {
+    public String getSearch(Model model1, Model model2, Model model3) {
 
       // Get the search results and generate an HTML page with them
 
+
+      //Hard-coded attributes for example sites to test thymeleaf
+      model1.addAttribute("name1", "Fake Site 1");
+      model1.addAttribute("price1", 0.01);
+      model1.addAttribute("description1", "fake site description");
+      model1.addAttribute("water1", "Has Water");
+      model1.addAttribute("electric1", "Has Electricity");
+      model1.addAttribute("reservation1", "Not currently reserved");
+      model2.addAttribute("name2", "Fake Site 2");
+      model2.addAttribute("price2", 0.02);
+      model3.addAttribute("name3", "Fake Site 3");
+      model3.addAttribute("price3", 0.03);
       return "search"; 
     } 
 
@@ -86,6 +100,14 @@ public class MainController {
 
       return response; 
     } 
+
+
+    //Thymeleaf test
+    @RequestMapping(method = RequestMethod.GET, value = "/thymeleaftest")
+    public String getThymeTest(Model model){
+      model.addAttribute("message", "main controller message");
+      return "thymeleaftest";
+    }
 
 
 }

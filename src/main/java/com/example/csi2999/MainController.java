daa26bc1513d10c7dfe87fd107953eae3be2b012
ecarnovsky <ecarnovsky@gmail.com>
2024-06-11 15:26:34 -0400

    Removed unneeded call to database

--- src/main/java/com/example/csi2999/MainController.java
+++ src/main/java/com/example/csi2999/MainController.java
@@ -25,7 +25,6 @@ public class MainController {
 
     @GetMapping("/")
     public String getStartup() {
-        System.out.println(supabaseClient.getSites());
         return "home";
     }
 
--- src/test/java/com/example/csi2999/Csi2999ApplicationTests.java
+++ src/test/java/com/example/csi2999/Csi2999ApplicationTests.java
@@ -1,7 +1,6 @@
 package com.example.csi2999;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
